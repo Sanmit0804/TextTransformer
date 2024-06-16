69fc5e865ee391ff41df2b7c76cb4182820fa9db
Sanmit Suthar <sanmitsuthar3@gmail.com>
2024-06-16 13:12:25 +0530

    Work on About Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/TextForm";
+// import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar name="Sanmit" title="React"></Navbar>
       <Textform></Textform>
+      {/* <About></About> */}
     </>
   );
 }
